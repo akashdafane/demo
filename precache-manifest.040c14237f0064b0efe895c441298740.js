23cbbba1e3d37028048d6d24fce6234ae2c6cdef
Akash Dafane <akashdafane18@gmail.com>
2020-08-02 18:34:22 +0530

    Updates

---
+++ precache-manifest.040c14237f0064b0efe895c441298740.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed31884d0d7774a4731e15d0bbb242c9",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "0b8010706b074903d28d",
+    "url": "/demo/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2115147be705896a3387",
+    "url": "/demo/static/css/main.3cb1bd8c.chunk.css"
+  },
+  {
+    "revision": "0b8010706b074903d28d",
+    "url": "/demo/static/js/2.c567da8a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/demo/static/js/2.c567da8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2115147be705896a3387",
+    "url": "/demo/static/js/main.6bde83be.chunk.js"
+  },
+  {
+    "revision": "b39b67791598b779560b",
+    "url": "/demo/static/js/runtime-main.a819b733.js"
+  },
+  {
+    "revision": "ad5aa2be9037eec558e4182949453c08",
+    "url": "/demo/static/media/logo.ad5aa2be.png"
+  }
+]);
